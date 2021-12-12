2d8a04ce2834bd66981d89a15e5e6dc24715566f
MiranDaniel@git:~$ <mirandanielcz@gmail.com>
2021-12-12 16:15:09 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,25 +1,37 @@
 cd ~/Downloads
-apt-get update -y
-apt-get upgrade -y
+apt update -y
+apt upgrade -y
+
+
+apt install iproute2
+apt install sudo
+
+
+apt install git
+apt install bash
+apt install neofetch
+apt install htop
+apt install net-tools
+apt install curl
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 # install fancy console (oh my bash)
 sed 's,OSH_THEME=[^;]*,OSH_THEME="mairan",' -i ~/.bashrc
 # config
 
-gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+{
+  gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+} || {}
 # add maximize button on Pop!_OS
 
-apt-get install neofetch
-apt-get install htop
-apt-get install net-tools
+
 
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt-get update
+apt update
 apt install gh
 
-apt-get install byobu
+apt install byobu
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 
